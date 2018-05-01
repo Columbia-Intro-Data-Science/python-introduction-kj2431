354a0f7878f42dea26b05a7875ee504011a99dd8
kj2431 <35583533+kj2431@users.noreply.github.com>
2018-05-01 19:51:12 -0400

    Add files via upload

---
+++ get_stock.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import random
+import json
+import math
+import time
+import datetime
+
+from pandas import datetime
+from pandas_datareader import data
+
+print('Enter a NYSE ticker symbol:')
+stock = input()
+
+start = datetime(2016,3,19)
+end = datetime(2018,3,26)
+
+df = data.DataReader(stock, 'iex', start, end)
+print(df.head())
+print(df.tail())
